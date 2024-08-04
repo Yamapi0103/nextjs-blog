43a074259d08e4f53600245a1e2d7b74bb7592fb
joshua yeh <yamapi0103@gmail.com>
2024-08-04 16:11:11 +0800

    add layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 import Head from 'next/head';
-
+import Layout from '../../components/layout';
 export default function FirstPost() {
-  return <>
+  return <Layout>
     <Head>
       <title>First Post</title>
     </Head>
@@ -11,5 +11,5 @@ export default function FirstPost() {
     <h2>
       <Link href="/">Back to home</Link>
     </h2>
-  </>
+  </Layout>
 }
